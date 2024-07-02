650361c9927e9cfc262114772a37aa99956fcdaf
RameenKhannnn <rameenkhan8787@gmail.com>
2024-07-01 22:58:12 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+alert("Thanks for your input!");
+var name = "Mark";
+name = "Ace";
+var nationality;
+nationality = "U.S.";
+var floogle = "Mark";
+var name = "Mark";
+var floogle = "Mark";
+var lessonAuthor = "Mark";
+var guyWhoKeepsSayingHisOwnName = "Mark";
+var x = "Mark";
+var lastName = "Smith";
+var cityOfOrigin = "New Orleans";
+var aussieGreeting = "g'Day";
+var nickname = "Bub";
+alert("Thanks for your input!");
+ var thanx = "Thanks for your input!"
+alert(thanx);
+var weight = 150;
+weight + 25
+ var originalNum = 23;
+ var newNum = originalNum + 7;
+ var originalNum = 23;
+ var numToBeAdded = 7;
+ var newNum = originalNum + numToBeAdded;
+var originalNum = 90;
+ originalNum = originalNum + 10;
+ var originalNum = "23";
+ var newNum = originalNum + 7;
+
